f2571f69580402d52a644bb40f944b20852800c6
islam <dujsobaevislam01@gmail.com>
2024-12-20 11:58:01 +0600

    13-commit

--- handlers/group_panel/user_group_private.py
+++ handlers/group_panel/user_group_private.py
@@ -9,8 +9,14 @@ user_group_router = Router()
 user_group_router.message.filter(ChatTypeFilter(['group', 'supergroup']))
 user_group_router.edited_message.filter(ChatTypeFilter(['group', 'supergroup']))
 
-restricted_words = {'кабан', 'хомяк', 'выхухоль'}
-
+restricted_words = {
+    'дурак', 'идиот', 'глупец', 'тупой', 'кретин', 'мразь', 'сволочь', 'урод',
+    'дебил', 'придурок', 'засранец', 'тварь', 'скотина', 'гнида', 'сука',
+    'падла', 'ублюдок', 'шлюха', 'проститутка', 'блядь', 'хрен', 'чмо',
+    'козёл', 'баран', 'осёл', 'мерзавец', 'жопа', 'пизда', 'мудак', 'лох',
+    'гандон', 'пидор', 'гей', 'тупица', 'недоумок', 'говно', 'задница',
+    'петух', 'курва', 'шваль', 'хренов', 'шалава', 'говнюк', 'козлина'
+}
 
 @user_group_router.message(Command("admin"))
 async def get_admins(message: types.Message, bot: Bot):
@@ -29,4 +35,16 @@ async def get_admins(message: types.Message, bot: Bot):
 def clean_text(text: str):
     return text.translate(str.maketrans('', '', punctuation))
 
+# Проверка сообщений на запрещённые слова
+@user_group_router.message()
+async def check_restricted_words(message: types.Message):
+    # Очистка текста от пунктуации и приведение к нижнему регистру
+    cleaned_text = clean_text(message.text)
+    message_words = set(cleaned_text.split())
 
+    # Проверка на наличие запрещённых слов
+    if restricted_words.intersection(message_words):
+        await message.delete()  # Удаление сообщения
+        await message.answer(
+            f"⚠️ {message.from_user.first_name}, использование запрещённых слов недопустимо."
+        )
